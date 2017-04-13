6fe237c2b21dfc7742fa4eab3db656bd68225e5e
Arthur Rump <arthurrump@outlook.com>
2017-04-13 19:25:26 +0200

    Add screensaver behaviour

--- 4OpEenRijScreensaver/MainWindow.xaml.cs
+++ 4OpEenRijScreensaver/MainWindow.xaml.cs
@@ -15,14 +15,30 @@ using System.Windows.Shapes;
 
 namespace _4OpEenRijScreensaver
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        Point _mousePos;
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_mousePos != default(Point) && _mousePos != e.GetPosition(MainWindowWindow))
+                App.Current.Shutdown();
+
+            _mousePos = e.GetPosition(MainWindowWindow);
         }
     }
 }
