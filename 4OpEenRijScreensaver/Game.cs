4ce788c797392907c75bb2fa85265abc7cb78596
Arthur Rump <arthurrump@outlook.com>
2017-04-15 21:00:38 +0200

    Refactor everything

---
+++ 4OpEenRijScreensaver/Game.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace _4OpEenRijScreensaver
+{
+    public class Game
+    {
+        const int EMPTY = 0;
+
+        int _cols, _rows, _playerCount;
+
+        public Game(int columns, int rows, int playerCount)
+        {
+            _cols = columns;
+            _rows = rows;
+            _playerCount = playerCount;
+        }
+
+        public async Task Start(Action<SolidColorBrush, int> draw, Func<(int column, int row)[], Task> blink, Action reset)
+        {
+            while (true)
+            {
+                await Play(draw, blink);
+                reset();
+            }
+        }
+
+        private async Task Play(Action<SolidColorBrush, int> draw, Func<(int column, int row)[], Task> blink)
+        {
+            SolidColorBrush[] playerColors = GetPlayerColors(_playerCount);
+            int[,] board = new int[_cols, _rows];
+
+            for (int i = 0; i < _cols * _rows; i++)
+            {
+                (int num, SolidColorBrush color) currentPlayer = (i % _playerCount + 1, playerColors[i % _playerCount]);
+
+                (int col, int row) = Next(ref board, currentPlayer.num);
+
+                draw(currentPlayer.color, col);
+
+                await Task.Delay(250);
+
+                var winningMove = IsWinningMove(board, col, row, currentPlayer.num);
+                if (winningMove.win)
+                {
+                    await blink(winningMove.fields);
+                    break;
+                }
+
+                await Task.Delay(250);
+            }
+        }
+
+        private (int column, int row) Next(ref int[,] board, int player)
+        {
+            int col, row = -1;
+            var random = new Random();
+
+            do
+            {
+                col = random.Next(_cols);
+            } while (board[col, _rows - 1] != EMPTY);
+
+            for (int r = 0; r < _rows; r++)
+            {
+                if (board[col, r] == EMPTY)
+                {
+                    board[col, r] = player;
+                    row = r;
+                    break;
+                }
+            }
+
+            return (col, row);
+        }
+
+        public (bool win, (int column, int row)[] fields) IsWinningMove(int[,] board, int column, int row, int player)
+        {
+            // Horizontal
+            for (int c = Math.Max(column - 3, 0); c <= column && c + 3 < _cols; c++)
+                if (board[c, row] == player && board[c + 1, row] == player &&
+                    board[c + 2, row] == player && board[c + 3, row] == player)
+                    return (true, new(int, int)[] { (c, row), (c + 1, row), (c + 2, row), (c + 3, row) });
+
+            // Vertical
+            for (int r = Math.Max(row - 3, 0); r <= row && r + 3 < _rows; r++)
+                if (board[column, r] == player && board[column, r + 1] == player &&
+                    board[column, r + 2] == player && board[column, r + 3] == player)
+                    return (true, new(int, int)[] { (column, r), (column, r + 1), (column, r + 2), (column, r + 3) });
+
+            // Diagonal Bottom-up
+            for (int d = Math.Min(column - 3, row - 3) < 0 ? -3 - Math.Min(column - 3, row - 3) : -3;
+                d <= 0 && column + d + 3 < _cols && row + d + 3 < _rows; d++)
+                if (board[column + d, row + d] == player && board[column + d + 1, row + d + 1] == player &&
+                    board[column + d + 2, row + d + 2] == player && board[column + d + 3, row + d + 3] == player)
+                    return (true, new(int, int)[] {
+                            (column + d, row + d), (column + d + 1, row + d + 1), (column + d + 2, row + d + 2), (column + d + 3, row + d + 3)
+                        });
+
+            // Diagonal Top-down
+            for (int d = Math.Min(column - 3, _rows - 1 - (row + 3)) < 0 ? -3 - Math.Min(column - 3, _rows - 1 - (row + 3)) : -3;
+                d <= 0 && column + d + 3 < _cols && row - d - 3 >= 0; d++)
+                if (board[column + d, row - d] == player && board[column + d + 1, row - d - 1] == player &&
+                    board[column + d + 2, row - d - 2] == player && board[column + d + 3, row - d - 3] == player)
+                    return (true, new(int, int)[] {
+                            (column + d, row - d), (column + d + 1, row - d - 1), (column + d + 2, row - d - 2), (column + d + 3, row - d - 3)
+                        });
+
+            return (false, null);
+        }
+
+        private SolidColorBrush[] GetPlayerColors(int count)
+        {
+            var r = new Random();
+            var result = new SolidColorBrush[count];
+
+            for (int i = 0; i < count; i++)
+                result[i] = new SolidColorBrush(Color.FromRgb((byte)r.Next(256), (byte)r.Next(256), (byte)r.Next(256)));
+
+            return result;
+        }
+    }
+}
--- 4OpEenRijScreensaver/MainWindow.xaml.cs
+++ 4OpEenRijScreensaver/MainWindow.xaml.cs
@@ -15,15 +15,6 @@ using System.Windows.Shapes;
 
 namespace _4OpEenRijScreensaver
 {
-    enum Field
-    {
-        Empty,
-        Zero,
-        One,
-        Two,
-        Three
-    }
-
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -31,36 +22,22 @@ namespace _4OpEenRijScreensaver
             InitializeComponent();
         }
 
-        int _cols, _rows;
-        double _r;
-        Field[,] _board;
-
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            _cols = 19; // TODO: decide based on actual screensize
-            _r = MainWindowWindow.ActualWidth / _cols;
-            _rows = (int)Math.Round(MainWindowWindow.ActualHeight / _r);
-            _board = new Field[_cols, _rows];
+            int cols = 19; // TODO: decide based on actual screensize
+            double radius = MainWindowWindow.ActualWidth / cols;
+            int rows = (int)Math.Round(MainWindowWindow.ActualHeight / radius);
 
-            var rand = new Random();
+            Game game = new Game(cols, rows, 4);
 
-            SolidColorBrush[] players =
-            {
-                new SolidColorBrush(
-                        Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256))
-                    ),
-                new SolidColorBrush(
-                        Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256))
-                    ),
-                new SolidColorBrush(
-                        Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256))
-                    ),
-                new SolidColorBrush(
-                        Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256))
-                    )
-            };
+            InitUI(cols);
 
-            for (int i = 0; i < _cols; i++)
+            await game.Start((color, column) => Draw(radius, color, column), BlinkFields, ResetUI);
+        }
+
+        private void InitUI(int columns)
+        {
+            for (int i = 0; i < columns; i++)
             {
                 MainGrid.ColumnDefinitions.Add(
                     new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
@@ -70,120 +47,52 @@ namespace _4OpEenRijScreensaver
                 s.SetValue(Grid.ColumnProperty, i);
                 MainGrid.Children.Add(s);
             }
+        }
 
-
-            for (int i = 0; i < _cols * _rows; i++)
+        private void Draw(double radius, SolidColorBrush color, int column)
+        {
+            var el = new Ellipse
             {
-                int col, row = -1;
-                Field player = (Field)(i % 4 + 1);
+                Fill = color,
+                Width = radius - 8.5,
+                Height = radius - 8.5,
+                Margin = new Thickness(4)
+            };
+            ((Panel)MainGrid.Children[column]).Children.Insert(0, el);
+        }
 
-                do
-                {
-                    col = rand.Next(_cols);
-                } while (_board[col, _rows - 1] != Field.Empty);
+        private async Task BlinkFields((int column, int row)[] fields)
+        {
+            // In Game (0,0) is the lower left corner, in the UI it's the highest ellipse in the left column
+            for (int j = 0; j < 4; j++)
+                fields[j].row = ((Panel)MainGrid.Children[fields[j].column]).Children.Count - 1 - fields[j].row;
 
-                for (int r = 0; r < _rows; r++)
-                    if (_board[col, r] == Field.Empty)
-                    {
-                        _board[col, r] = player;
-                        row = r;
-                        break;
-                    }
+            UIElement[] ellipses =
+            {
+                ((Panel)MainGrid.Children[fields[0].column]).Children[fields[0].row],
+                ((Panel)MainGrid.Children[fields[1].column]).Children[fields[1].row],
+                ((Panel)MainGrid.Children[fields[2].column]).Children[fields[2].row],
+                ((Panel)MainGrid.Children[fields[3].column]).Children[fields[3].row]
+            };
 
-                var el = new Ellipse
-                {
-                    Fill = players[(int)player - 1],
-                    Width = _r - 8.5,
-                    Height = _r - 8.5,
-                    Margin = new Thickness(4)
-                };
-                ((Panel)MainGrid.Children[col]).Children.Insert(0, el);
+            foreach (var ell in ellipses) ell.Opacity = 1;
+            await Task.Delay(500);
+            foreach (var ell in ellipses) ell.Opacity = 0;
+            await Task.Delay(400);
+            foreach (var ell in ellipses) ell.Opacity = 1;
+            await Task.Delay(300);
+            foreach (var ell in ellipses) ell.Opacity = 0;
+            await Task.Delay(200);
+            foreach (var ell in ellipses) ell.Opacity = 1;
+            await Task.Delay(100);
+            foreach (var ell in ellipses) ell.Opacity = 0;
+            await Task.Delay(500);
+        }
 
-                await Task.Delay(250);
-
-                // Check for 4-in-a-row around new filled field
-                // Horizontal
-                for (int c = Math.Max(col - 3, 0); c <= col && c + 3 < _cols; c++)
-                    if (_board[c, row] == player && _board[c + 1, row] == player &&
-                        _board[c + 2, row] == player && _board[c + 3, row] == player)
-                        await Winner(new(int, int)[] { (c, row), (c + 1, row), (c + 2, row), (c + 3, row) });
-
-                // Vertical
-                for (int r = Math.Max(row - 3, 0); r <= row && r + 3 < _rows; r++)
-                    if (_board[col, r] == player && _board[col, r + 1] == player &&
-                        _board[col, r + 2] == player && _board[col, r + 3] == player)
-                        await Winner(new(int, int)[] { (col, r), (col, r + 1), (col, r + 2), (col, r + 3) });
-
-                // Diagonal Bottom-up
-                for (int d = Math.Min(col - 3, row - 3) < 0 ? -3 - Math.Min(col - 3, row - 3) : -3;
-                    d <= 0 && col + d + 3 < _cols && row + d + 3 < _rows; d++)
-                    if (_board[col + d, row + d] == player && _board[col + d + 1, row + d + 1] == player &&
-                        _board[col + d + 2, row + d + 2] == player && _board[col + d + 3, row + d + 3] == player)
-                        await Winner(new(int, int)[] {
-                            (col + d, row + d), (col + d + 1, row + d + 1), (col + d + 2, row + d + 2), (col + d + 3, row + d + 3)
-                        });
-
-                // Diagonal Top-down
-                for (int d = Math.Min(col - 3, _rows - 1 - (row + 3)) < 0 ? -3 - Math.Min(col - 3, _rows - 1 - (row + 3)) : -3;
-                    d <= 0 && col + d + 3 < _cols && row - d - 3 >= 0; d++)
-                    if (_board[col + d, row - d] == player && _board[col + d + 1, row - d - 1] == player &&
-                        _board[col + d + 2, row - d - 2] == player && _board[col + d + 3, row - d - 3] == player)
-                        await Winner(new(int, int)[] {
-                            (col + d, row - d), (col + d + 1, row - d - 1), (col + d + 2, row - d - 2), (col + d + 3, row - d - 3)
-                        });
-
-                await Task.Delay(250);
-
-                async Task Winner((int c, int r)[] win)
-                {
-                    // In _board (0,0) is the lower left corner, in the UI it's the highest ellipse in the left column
-                    for (int j = 0; j < 4; j++)
-                        win[j].r = ((Panel)MainGrid.Children[win[j].c]).Children.Count - 1 - win[j].r;
-
-                    UIElement[] ellipses =
-                    {
-                        ((Panel)MainGrid.Children[win[0].c]).Children[win[0].r],
-                        ((Panel)MainGrid.Children[win[1].c]).Children[win[1].r],
-                        ((Panel)MainGrid.Children[win[2].c]).Children[win[2].r],
-                        ((Panel)MainGrid.Children[win[3].c]).Children[win[3].r]
-                    };
-
-                    foreach (var ell in ellipses) ell.Opacity = 1;
-                    await Task.Delay(500);
-                    foreach (var ell in ellipses) ell.Opacity = 0;
-                    await Task.Delay(400);
-                    foreach (var ell in ellipses) ell.Opacity = 1;
-                    await Task.Delay(300);
-                    foreach (var ell in ellipses) ell.Opacity = 0;
-                    await Task.Delay(200);
-                    foreach (var ell in ellipses) ell.Opacity = 1;
-                    await Task.Delay(100);
-                    foreach (var ell in ellipses) ell.Opacity = 0;
-                    await Task.Delay(500);
-
-                    _board = new Field[_cols, _rows];
-                    i = 0;
-
-                    foreach (Panel p in MainGrid.Children)
-                        p.Children.Clear();
-
-                    players = new SolidColorBrush[]
-                    {
-                        new SolidColorBrush(
-                                Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256))
-                            ),
-                        new SolidColorBrush(
-                                Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256))
-                            ),
-                        new SolidColorBrush(
-                                Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256))
-                            ),
-                        new SolidColorBrush(
-                                Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256))
-                            )
-                    };
-                }
-            }
+        private void ResetUI()
+        {
+            foreach (Panel p in MainGrid.Children)
+                p.Children.Clear();
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
